9c29ee8c3abdba2ec17fd124d9944bd6e475bafc
Steve Buckley <buckleys78@gmail.com>
2014-04-23 17:46:43 -0400

    S.Buckley: needs refactoring, but works

--- monsters.rb
+++ monsters.rb
@@ -42,13 +42,32 @@ puts $monsters.count{ |m| m[:legs] == 2 }
 #puts $monsters.select{|m| m[:legs]== 2}.map{|m| m[:name]}
 
 puts "How many monsters are nocturnal?"
-puts # your answer here
+puts $monsters.count{ |m| m[:nocturnal]}
 puts "What are the names of the monsters that are nocturnal?"
-puts # your answer here
+puts $monsters.select{|m| m[:nocturnal]}.map{|m| m[:name]}
 puts "How many legs do all our monsters have?"
-puts # your answer here
+puts $monsters.inject(0) {|sum,m| sum + m[:legs]}
 puts "What are the 2 most common dangers of our monsters?"
-puts # your answer here
+h = Hash.new(0)
+$monsters.each do |m|
+  m[:dangers].each do |d|
+    h[d] +=1
+  end
+end
+top_danger = h.max_by {|k, v| v } [0]
+h2 = h.clone
+h2.delete(top_danger)
+second_danger = h2.max_by {|k, v| v } [0]
+puts "Top danger: #{top_danger}, Next danger: #{second_danger}"
 puts "What are the 2 most common vulnerabilities of our monsters?"
-puts # your answer here
-
+h = Hash.new(0)
+$monsters.each do |m|
+  m[:vulnerabilities].each do |d|
+    h[d] +=1
+  end
+end
+top_vulnerability = h.max_by {|k, v| v } [0]
+h2 = h.clone
+h2.delete(top_vulnerability)
+second_vulnerability = h2.max_by {|k, v| v } [0]
+puts "Top vulnerability: #{top_vulnerability}, Next vulnerability: #{second_vulnerability}"
